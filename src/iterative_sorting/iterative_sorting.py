0edf7a0dc7a3629035f0a9ade050409b6c5092e0
Kevin Stewart <kistewart419@gmail.com>
2020-09-30 22:56:26 -0400

    Code complete but tests still not passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -8,16 +8,45 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
 
+        # for x in the range of the current index + 1 (moving right) within the array:
+        for smallest_index in range(i + 1, len(arr)):
+            # if the smallest index in the array is lower than x in the array:
+            if arr[smallest_index] < arr[cur_index]:
+                # the smallest index is equal to x
+                cur_index = smallest_index
 
         # TO-DO: swap
         # Your code here
+        # current index in the array is now the smallest index in the array
+            arr[i], arr[cur_index] = arr[cur_index], arr[i]
 
     return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+
+    # length is the length of the array
+    length = len(arr)
+    # for i in the range of the length
+    for i in range(length):
+        # boolean swapped variable that is set to false
+        swapped: bool = False
+
+        compared_range = length - i - 1
+        # for x in the range of the array -1 (moving left) of the current index:
+        for x in range(0, compared_range) :
+            # next index is current index + 1 (moving right)
+            next_index = x + 1
+            # if x index is lower than the next index
+            if arr[x] > arr[next_index]:
+                # swap values
+                arr[x], arr[next_index] = arr[next_index], arr[x]
+                # swapped is set to true
+                swapped = True
+    # if swapped is equal to false function stops
+            elif swapped == False:
+                pass      
 
 
     return arr
